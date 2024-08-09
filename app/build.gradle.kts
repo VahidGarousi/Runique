8912fabbf8964146135e27db64ec600d0182b473
Vahid Garousi <garousiv@gmail.com>
2024-08-09 07:31:01 +0330

    Adding AndroidApplicationConventionPlugin for #7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,11 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.runique.android.application)
 }
 
 android {
     namespace = "ir.runique"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "ir.runique"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -29,18 +21,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
---
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,27 @@
+import com.android.build.api.dsl.ApplicationExtension
+import ir.runique.buildlogic.configureKotlinAndroid
+import ir.runique.buildlogic.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+internal class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        project.run {
+            pluginManager.run {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+            extensions.configure<ApplicationExtension> {
+                defaultConfig {
+                    applicationId = libs.findVersion("projectApplicationId").get().toString()
+                    targetSdk = libs.findVersion("projectTargetSdkVersion").get().toString().toInt()
+
+                    versionCode = libs.findVersion("projectVersionCode").get().toString().toInt()
+                    versionName = libs.findVersion("projectVersionName").get().toString()
+                }
+                configureKotlinAndroid(this)
+            }
+        }
+    }
+}
